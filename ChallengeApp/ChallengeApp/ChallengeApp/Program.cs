d32736e28da2365459c38222bd1bbeac58d6d760
Skaza Waclaw <skaza.ws@gmail.com>
2023-09-03 23:33:24 +0200

    dzien kolejny (4)

--- ChallengeApp/ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/ChallengeApp/Program.cs
@@ -1,6 +1,8 @@
 ﻿// liczby calkowite
 
-int myAge = 120;
+using System.ComponentModel.Design;
+
+int myAge = 40;
 int newAge = myAge + 5;
 
 int myVar = int.MaxValue;
@@ -25,4 +27,58 @@ var result2 = name.ToArray();
 bool isAktive = true;
 isAktive = false;
 var isValid = 5 > 6;
-Console.WriteLine(isValid);
+
+//Console.WriteLine(isValid);
+
+
+
+
+var number1 = 50;
+var number2 = 10;
+if (number1 < number2)
+{
+    //Console.WriteLine("JESTEM TUTAJ W LINI NR 34");
+}
+else
+{
+
+    // Console.WriteLine("jestem tutaj w lini nr 43");
+}
+
+// Operatory Relacyjne 
+//== rowna sie 
+//!= czy sa rozne 
+//< wieksza liczba 
+//> mniejsza liczba
+//< = lub rowne 
+//> = lub rowne 
+
+//Operatory logiczne 
+// && i 
+// || lub 
+//(!(...) negacja sprawdzanie czy nie jest mniejszy niz....
+
+var name4 = "Waclaw ";
+var age = 40;
+
+if (name4 == "Waclaw" && age < 50)
+
+{
+    if (name4 == "Waclaw")
+    {
+        Console.WriteLine("tak ma na inie Waclaw");
+    }else
+
+
+
+
+
+
+
+    Console.WriteLine("Jestem Waclawem lub mam niej niz 50 lat.");
+}
+
+else if (age > 39)
+{
+    Console.WriteLine("Jestem w innym wieku  ");
+}
